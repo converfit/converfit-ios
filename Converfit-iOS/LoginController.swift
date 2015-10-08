c2f0f450f2ed304c45700044c7076b87ed3bc331
Manuel Martinez Gomez <Manuel@MacBook-Pro-de-Manuel.local>
2015-10-08 11:22:35 +0200

    redondear vistas en pantalla login

--- Converfit-iOS/LoginController.swift
+++ Converfit-iOS/LoginController.swift
@@ -6,8 +6,40 @@
 //  Copyright © 2015 Citious Team SL. All rights reserved.
 //
 
-import Cocoa
+import UIKit
 
 class LoginController: UIViewController {
+    
+    //MARK: - Variables
+    
+    
+    //MARK: - Outlets
+    @IBOutlet weak var viewEmailPassword: UIView!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var lostPasswordLabel: UILabel!
+    @IBOutlet weak var singUpLabel: UILabel!
+    @IBOutlet weak var emailTxt: UITextField!
+    @IBOutlet weak var passwordTxt: UITextField!
+    
+    //MARK: - Actions
+    @IBAction func viewContainerTap(sender: AnyObject) {
+        dissmisKeyBoard()
+    }
 
+    //MARK: - LifeCycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        roundViews()
+    }
+    
+    //MARK: - KeyBoard
+    func dissmisKeyBoard(){
+        self.view.endEditing(true)
+    }
+    
+    //MARK: - Round views
+    func roundViews(){
+        viewEmailPassword.layer.cornerRadius = 5
+        loginButton.layer.cornerRadius = 5
+    }
 }
