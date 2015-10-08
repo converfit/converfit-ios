437690a10eaedd435d8ea221d267f63d16914089
Manuel Martinez Gomez <Manuel@MacBook-Pro-de-Manuel.local>
2015-10-08 11:21:37 +0200

    redondear vistas en pantalla login

---
+++ Converfit-iOS/LoginController.swift
@@ -0,0 +1,13 @@
+//
+//  LoginController.swift
+//  Converfit-iOS
+//
+//  Created by Manuel Martinez Gomez on 8/10/15.
+//  Copyright © 2015 Citious Team SL. All rights reserved.
+//
+
+import Cocoa
+
+class LoginController: UIViewController {
+
+}
