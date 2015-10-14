e411eeec7111426d7aaf976bc5d1f52e8ef57236
Manuel Martinez Gomez <Manuel@MacBook-Pro-de-Manuel.local>
2015-10-14 11:46:14 +0200

    Primera version collection view

---
+++ Converfit-iOS/TimeLineCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  TimeLineCollectionViewCell.swift
+//  Converfit-iOS
+//
+//  Created by Manuel Martinez Gomez on 14/10/15.
+//  Copyright © 2015 Citious Team SL. All rights reserved.
+//
+
+import UIKit
+
+class TimeLineCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var avatarImage: UIImageView!
+    @IBOutlet weak var userName: UILabel!
+    @IBOutlet weak var time: UILabel!
+    @IBOutlet weak var html: UILabel!
+    
+}
---
+++ Converfit-iOS/TimeLineViewController.swift
@@ -0,0 +1,56 @@
+//
+//  TimeLineViewController.swift
+//  Converfit-iOS
+//
+//  Created by Manuel Martinez Gomez on 14/10/15.
+//  Copyright © 2015 Citious Team SL. All rights reserved.
+//
+
+import UIKit
+
+class TimeLineViewController: UIViewController, UICollectionViewDataSource {
+    
+    private let reuseIdentifier = "timeLineCollectionCell"
+    private let sectionInsets = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.automaticallyAdjustsScrollViewInsets = false
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    //MARK: - CollectionViewDataSource
+    func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier(reuseIdentifier, forIndexPath: indexPath) as! TimeLineCollectionViewCell
+        
+        //cell.backgroundColor = UIColor.redColor()
+        
+        return cell
+    }
+}
+
+extension TimeLineViewController: UICollectionViewDelegateFlowLayout{
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
+        let width = self.view.frame.width
+        print(width)
+        return CGSize(width: width, height: 156)
+    }
+    
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAtIndex section: Int) -> UIEdgeInsets {
+        return sectionInsets
+    }
+}
