5e4455c28effd45b6d80d3a9f50bc0c18275eaca
Manuel Martinez Gomez <Manuel@MacBook-Pro-de-Manuel.local>
2015-10-14 14:53:43 +0200

    Evitar que al pulsar un link reabra cambie el webView en el collectionview

--- Converfit-iOS/TimeLineViewController.swift
+++ Converfit-iOS/TimeLineViewController.swift
@@ -189,6 +189,7 @@ class TimeLineViewController: UIViewController, UICollectionViewDataSource,UICol
     func webView(webView: UIWebView, shouldStartLoadWithRequest request: NSURLRequest, navigationType: UIWebViewNavigationType) -> Bool {
         if(navigationType == UIWebViewNavigationType.LinkClicked){
             UIApplication.sharedApplication().openURL(request.URL!)
+            return false
         }
         return true
     }
