a55b96e20d1331b56ae61040279ae7eda94cfeb2
manuelcitious <manuel_citious@iMac-de-Manuel.local>
2015-10-16 10:19:18 +0200

    Añadido VC para el menu lateral

---
+++ Converfit-iOS/LefMenuWiewController.swift
@@ -0,0 +1,18 @@
+//
+//  LefMenuWiewController.swift
+//  Converfit-iOS
+//
+//  Created by Manuel Citious on 16/10/15.
+//  Copyright © 2015 Citious Team SL. All rights reserved.
+//
+
+import UIKit
+
+class LefMenuWiewController: UIViewController {
+
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        //self.view.backgroundColor = Utils.returnRedConverfit()
+    }
+    
+}
